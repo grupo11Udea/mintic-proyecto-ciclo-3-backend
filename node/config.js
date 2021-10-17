6e293ee67e222caadd6c9aa6bb63a2c1603e1d8f
[monoedi15] <[Your>
2021-10-17 16:27:38 -0500

    cambiar visibilidad de config.js

--- node/config.js
+++ node/config.js
@@ -5,8 +5,11 @@ module.exports ={
         password: "0ba62628",
         database: "heroku_2688cdc66b09434",
         host: "us-cdbr-east-02.cleardb.com"
-
-        
-
     }
+/*     db:{
+        username: "root",
+        password: "",
+        database: "misionticotro",
+        host: "localhost"
+    } */
 }
